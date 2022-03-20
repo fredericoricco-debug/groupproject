392efa5e4d10904eb95613aee0d18d8baeb45cbd
Artem Rozinskiy <rozinskiy.artem@icloud.com>
2022-03-20 15:47:29 +0000

    Added tests for user class

--- flags/ctf/tests.py
+++ flags/ctf/tests.py
@@ -1,3 +1,27 @@
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+
 from django.test import TestCase
 
-# Create your tests here.
+
+class User_test(TestCase):
+
+    def setUp(self):
+        self.user_1 = User.objects.create_user("user1", "email@gmail.com", "password")
+        pass
+
+    def tearDown(self):
+        self.user_1.delete()
+        pass
+
+    def test_username(self):
+        self.assertEquals(self.user_1.get_username(), 'user1')
+
+    def test_email(self):
+        self.assertEquals(self.user_1.email, 'email@gmail.com')
+
+    def test_password(self):
+        self.assertTrue(self.user_1.check_password('password'))
+
+    def test_signIn(self):
+        self.assertTrue(authenticate(username="user1", password="password"))
